2aaf4441cb4bfd026a0cbc88fd3b01ab213a8511
Lucas Cardellino <91224219+lcardellino@users.noreply.github.com>
2022-11-16 18:32:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,85 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 8080;
+const logger = require("morgan");
 
-//========================================
-//    EJERCICIO 1
-//========================================
-app.get('/:nombre/:apellido', (req, res) => {
-  res.send(`Hola ${req.params.nombre} ${req.params.apellido}`)
-});
+app.use(logger("dev"));
+app.use(express.json());
 
-//========================================
-//    EJERCICIO 2
-//========================================
+const indexRouter = require("./routers/index");
+app.use("/", indexRouter);
 
-app.get('/dividir/:dividendo/:divisor/', (req, res) => {
-  let divisor = Number(req.params.divisor);
-  let dividendo = Number(req.params.dividendo);
-  
-  if (divisor == 0) {
-    res.json({ error: 'No se puede dividir por cero'})
-  }
-  else {
-    let resultado = ( dividendo / divisor)
-    res.json({resultado})  
-  }
-});
+const userRouter = require("./routers/users");
+app.use("/users", userRouter);
 
-//========================================
-//    EJERCICIO 3
-//========================================
+const mathRouter = require("./routers/math");
+app.use("/math", mathRouter);
 
-app.get('/suma/:num1/:num2/', (req, res) => {
-  let num1= Number(req.params.num1)
-  let num2= Number(req.params.num2)
+const listRouter = require("./routers/list");
+app.use("/list", listRouter);
 
-  if (num1 < 0 || num2 < 0) {
-    res.json({
-      error: 'no se puede enviar numero menores a cero'
-    })
-  }
-  else {
-    let resultado = num1 + num2
-    res.json({resultado})
-  }
-});
+const logRouter = require("./routers/log");
+app.use("/log", logRouter);
 
-//========================================
-//    EJERCICIO 4
-//========================================
 
-app.get('/autorizacion/', (req, res) => {
+module.exports = app;
 
-  let queryValue = Object.values(req.query)
-  let queryNumber = Number(queryValue)
 
-  if (queryNumber == 0 || queryNumber % 2 == 0) {
-    res.send('Autorizado') 
-  }
-  else {
-    res.send('No autorizado')
-  }
-});
-
-//========================================
-//    EJERCICIO 5
-//========================================
-
-app.get('/lista', (req, res) => {
-
-  let productos = {
-  producto1: req.query.product1,
-  producto2: req.query.product2,
-  producto3: req.query.product3,
-  producto4: req.query.product4,
-  producto5: req.query.product5,
-}
-res.json(productos)
-});
-
-//========================================
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
